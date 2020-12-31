7663b8da1f14798b555250ab4adf4e93b521847b
coleb <cole.ballard97@gmail.com>
2020-12-30 18:26:45 -0600

    added support to upload and download files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.querySelector("#input-file").addEventListener("change", getFile);
 document.querySelector("#password").addEventListener("keydown", checkKeyPressed, false);
 
 function alreadyHaveFile() {
@@ -9,6 +10,53 @@ function makeNewFile() {
     hideElement("#access-file");
     showElement("#enter-password");
 }
+
+function getFile(event) {
+	const input = event.target;
+    if ("files" in input && input.files.length > 0)
+        readFileContent(input.files[0]).then(content => {
+            document.querySelector("#content-target").value = content;
+            save("pojs.txt", content);
+        }).catch(error => console.log(error));
+    else;
+}
+
+function readFileContent(file) {
+	const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+    reader.onload = event => resolve(event.target.result);
+    reader.onerror = error => reject(error);
+    reader.readAsText(file);
+    });
+}
+
+function save(filename, data) {
+    var blob = new Blob([data], {type: 'text/csv'});
+    if (window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        var elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;        
+        document.body.appendChild(elem);
+        elem.click();        
+        document.body.removeChild(elem);
+    }
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
 
 function checkKeyPressed(event) {
     if (event.keyCode == "13") enterPassword();
